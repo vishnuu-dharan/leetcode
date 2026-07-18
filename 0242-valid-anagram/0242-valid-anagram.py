974e7f693f4bd9f970f56d45805fb3b5e265779c
vishnu dharan <vishnudharanktv25005@gmail.com>
2026-07-18 15:53:39 +0530

    Time: 19 ms (16.14%), Space: 20.3 MB (18.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,3 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        s=sorted(list(s))
-        t=sorted(list(t))
-        #print(,b)
-        return s==t
-        
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+       return sorted(s) == sorted(t)
