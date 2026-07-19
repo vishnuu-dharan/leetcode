d6938063e3fd3488631b8a5bad9db9f73335a928
vishnu dharan <vishnudharanktv25005@gmail.com>
2026-07-19 20:44:08 +0530

    Time: 0 ms (100%), Space: 12.2 MB (91.65%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def spiralOrder(self,
+    matrix):
+        res = []
+
+        while matrix:
+
+            res += matrix.pop(0)
+
+            if matrix and matrix[0]:
+                for row in matrix:
+                    res.append(row.pop())
+
+            if matrix:
+                res += matrix.pop()[::-1]
+
+            if matrix and matrix[0]:
+                for row in matrix[::-1]:
+                    res.append(row.pop(0))
+
+        return res
+
